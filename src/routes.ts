114f325c385e02b1688fd4ea4687457371afc1df
Carlos <tiagocdz29@hotmail.com>
2024-09-05 16:17:48 -0400

    Criação das rotas de categorias e produtos

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,11 @@ import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+import { ListcategoryController } from "./controllers/category/ListcategoryController";
+
+import { CreateProductController } from "./controllers/product/CreateProductController";
+
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 const router = Router();
@@ -14,5 +19,13 @@ router.post('/users', new CreateUserController().handle);
 router.post('/session', new AuthUserController().handle);
 
 router.get('/me', isAuthenticated, new DetailUserController().handle); 
+
+//Rotas Category
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
+
+router.get('/category', isAuthenticated, new ListcategoryController().handle)
+
+//Rotas product
+router.post('/product', isAuthenticated, new CreateProductController().handle)
 
 export { router };
